5676f7df45ddf193e9659b7fa038b91fb7226d7e
fateking27 <fateking27@outlook.com>
2023-09-08 16:44:02 +0800

    add

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,11 @@
-import { legasy_createStore, combineReducers } from 'redux'
+import { legacy_createStore, combineReducers } from 'redux'
 import rolesReducer from './role/reducers'
-const store = legasy_createStore()
+
+// 合并所有模块的 reducer 函数
+const allReducers = combineReducers({
+    roles: rolesReducer,
+})
+
+const store = legacy_createStore(allReducers);
+
 export default store
